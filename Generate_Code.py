7f731d23363aa3a38e113a0bb4e383bac8b5a2aa
Fernandohka <fernando2005andrade@gmail.com>
2025-05-26 14:55:13 -0300

    Generate Code

---
+++ Generate_Code.py
@@ -0,0 +1,23 @@
+import uuid
+import hashlib
+
+def get_mac_address():
+  mac_int = uuid.getnode()
+  mac_hex = hex(mac_int)[2:].upper()
+  return ":".join(mac_hex[i:i+2] for i in range(0, 12, 2))
+
+def criar_codigo_unico(mac_address):
+    mac_address_bytes = mac_address.encode('utf-8')
+
+    hash_object = hashlib.sha256(mac_address_bytes)
+    hash_hex = hash_object.hexdigest()
+
+    return hash_hex
+
+def generateCode():
+    mac_address = get_mac_address()
+    print(f"MAC Address: {mac_address}")
+    return criar_codigo_unico(mac_address)
+
+codigo_unico = generateCode()
+print(f"Código Único: {codigo_unico}")
