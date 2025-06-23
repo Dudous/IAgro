2f4229c13d3534be8f6580b06c4a52f4b3eadd1d
Fernandohka <fernando2005andrade@gmail.com>
2025-06-23 17:18:28 -0300

    Depois termino

--- script.py
+++ script.py
@@ -1,8 +1,30 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
+# import http.server
+# import socketserver
+# import webbrowser
+# import os
+
+# import webbrowser
+
+# url = 'http://127.0.0.1:5500/index.html'
+# webbrowser.open(url)
+
+# Handler = http.server.SimpleHTTPRequestHandler
+
+# with socketserver.TCPServer(("", 5500), Handler) as httpd:
+    
+#     webbrowser.open_new_tab(f"http://127.0.0.1:{5500}/index.html") 
+
+#     httpd.serve_forever()
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/location', methods=['POST'])
+@cross_origin()
 def location():
     data = request.get_json()
     print('Latitude:', data['latitude'])
