a0b6f2b9402d0ffa5675c387be98af41e43ffe69
Fernandohka <fernando2005andrade@gmail.com>
2025-07-01 17:18:33 -0300

    ...?

--- script.py
+++ script.py
@@ -1,23 +1,30 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
-# import http.server
-# import socketserver
-# import webbrowser
-# import os
+import http.server
+import socketserver
+import webbrowser
+import os
+import threading
+import webbrowser
 
-# import webbrowser
 
-# url = 'http://127.0.0.1:5500/index.html'
-# webbrowser.open(url)
+def openBrowser():
+    url = 'http://127.0.0.1:5500/index.html'
+    webbrowser.open(url)
 
-# Handler = http.server.SimpleHTTPRequestHandler
+    Handler = http.server.SimpleHTTPRequestHandler
 
-# with socketserver.TCPServer(("", 5500), Handler) as httpd:
-    
-#     webbrowser.open_new_tab(f"http://127.0.0.1:{5500}/index.html") 
+    with socketserver.TCPServer(("", 5500), Handler) as httpd:
+        
+        webbrowser.open_new_tab(f"http://127.0.0.1:{5500}/index.html") 
 
-#     httpd.serve_forever()
+        httpd.serve_forever()
+        
+thread = threading.Thread(target=openBrowser)
+thread.start()
+
+
 
 
 app = Flask(__name__)
