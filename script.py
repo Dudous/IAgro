df32fa60d8187d94bb31dda125959da48583cd26
Fernandohka <fernando2005andrade@gmail.com>
2025-07-03 14:50:17 -0300

    script abre o index

--- script.py
+++ script.py
@@ -1,31 +1,24 @@
+import os
+import webbrowser
+import threading
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
-
 import http.server
 import socketserver
-import webbrowser
-import os
-import threading
-import webbrowser
 
-
-def openBrowser():
+def open_browser():
     url = 'http://127.0.0.1:5500/index.html'
     webbrowser.open(url)
-
+    
     Handler = http.server.SimpleHTTPRequestHandler
-
+    
     with socketserver.TCPServer(("", 5500), Handler) as httpd:
-        
-        webbrowser.open_new_tab(f"http://127.0.0.1:{5500}/index.html") 
-
+        print(f"Serving static files on http://127.0.0.1:5500")
         httpd.serve_forever()
-        
-thread = threading.Thread(target=openBrowser)
+
+thread = threading.Thread(target=open_browser)
+thread.daemon = True
 thread.start()
-
-
-
 
 app = Flask(__name__)
 CORS(app)
@@ -36,8 +29,9 @@ def location():
     data = request.get_json()
     print('Latitude:', data['latitude'])
     print('Longitude:', data['longitude'])
-    print(data)
+    print('Data received:', data)
     return 'OK', 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    print("Starting Flask app on http://127.0.0.1:5000")
+    app.run(debug=False, port=5000)
